5dbd18890dd78912fcd9e9e1cdf56483db8fe43b
VictorD2 <59150886+VictorD2@users.noreply.github.com>
2023-10-29 17:34:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,35 +4,12 @@ from flask_cors import CORS
 from flask import Flask
 from waitress import serve
 
+load_dotenv()
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(chat_bp)
 
-class App:
-    def __init__(self, port=4001):
-        load_dotenv()
-        self.port = port
-        self.app = Flask(__name__)
-        self.config()
-        self.routes()
+if __name__ == '__main__':
+    # serve(self.app, port=self.port)
+    app.run(port = 4003, debug = True)
 
-    def config(self):
-        CORS(self.app)
-        # self.app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-        # self.app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
-
-
-    def routes(self):
-        # self.app.register_blueprint(contact_bp)
-        # self.app.register_blueprint(folder_bp)
-        # self.app.register_blueprint(class_bp)
-        # self.app.register_blueprint(user_bp)
-        # self.app.register_blueprint(auth_bp)
-        self.app.register_blueprint(chat_bp)
-
-    def run(self):
-        if __name__ == "__main__":
-            print("SERVER ON PORT " + str(self.port))
-            serve(self.app, port=self.port)
-            print("SERVER ON PORT " + str(self.port))
-            # self.app.run(port=self.port, debug=True)
-
-
-App().run()
